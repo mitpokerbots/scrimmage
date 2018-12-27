ea63b86863ce34a164ad6a49348a98c9e0c1f19c
Jack Serrino <Jserrino@gmail.com>
2018-12-27 13:16:40 -0600

    Update requirements, add manage.py, add migrations

--- runserver.py
+++
@@ -1,8 +0,0 @@
-import os
-from scrimmage import app, db
-
-if not os.path.exists('dev.db'):
-  print "Regenerating db..."
-  db.create_all()
-
-app.run()
--- scrimmage/__init__.py
+++ scrimmage/__init__.py
@@ -2,12 +2,13 @@ import os
 from celery import Celery
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.contrib.cache import SimpleCache
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 config_object_str = 'scrimmage.config.ProdConfig' if os.environ.get('PRODUCTION', False) else 'scrimmage.config.DevConfig'
 app.config.from_object(config_object_str)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 def make_celery(flask_app):
     celery = Celery(flask_app.import_name, broker=flask_app.config['CELERY_BROKER_URL'])
@@ -22,13 +23,7 @@ def make_celery(flask_app):
     return celery
 
 
-def make_cache(flask_app):
-    # Use RedisCache later
-    return SimpleCache()
-
-
 celery_app = make_celery(app)
-cache = make_cache(app)
 
 import scrimmage.user
 import scrimmage.admin
